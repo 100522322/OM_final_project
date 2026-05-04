8da846be3386d88d86fb37e5d33cbac53cbea74d
Hugo Pastrana <hugo.pastrana.torres@gmail.com>
2026-05-04 08:39:27 +0200

    update gitignore and main for testing graph/node

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include "Graph.h"
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
-    return 0;
+
+    Graph graph = Graph();
+
+    graph.load_from_file("instances/DSJC125.1.col");
+
+    std::cout << "There are " << graph.num_nodes<< " nodes and " << graph.num_edges << " edges" << std::endl;
+
+    for (Node node : graph.nodes) {
+        std::cout << "Node_id:"<<node.id << std::endl;
+    }
+
 }
